f3c1bf22f774fde1bd20c163a5a2082d0c82745e
NaN <mauriciodominguez9876543@gmail.com>
2024-06-06 18:41:48 -0300

    Registros terminado
    Registro refactorizado

--- js/registro.js
+++ js/registro.js
@@ -1,4 +1,5 @@
-import { enfocarInputsAnterior } from "./formularioAux.js";
+//imports
+import { contengaSoloLetras, validarQueSeaCorreo, queContengaLetrasYNumeros, validarContrasenia, validarNumeroTarjeta, validarClaveCVV } from "./formularioAux.js";
 // traer el formulario a validar
 
 const formulario = document.querySelector(".formulario");
@@ -7,9 +8,18 @@ const formulario = document.querySelector(".formulario");
 
 formulario.addEventListener("submit", (e)=>{
 
-    const inputs = document.querySelectorAll('.espacio');
+    if(validaRegistro() && validaMetodoDePago()){ 
 
-    //traer los campos a validar
+        formulario.submit();
+    }else{
+        e.preventDefault();
+    }
+});
+
+//funciones que deben ir si o si en registro
+
+function validaRegistro(){
+    //variables del registro
     let nombre= document.getElementById("nombre").value;
     let apellido= document.getElementById("apellido").value;
 
@@ -20,84 +30,35 @@ formulario.addEventListener("submit", (e)=>{
     let contrasenia= document.getElementById("password").value;
     let repeatContrasenia = document.getElementById("repeatpassword").value;
 
+    let contraseniasIguales=true;
+
+    if(!(contrasenia==repeatContrasenia)){
+        //done: aparece mensaje si son diferentes 
+        alert ("Las contraseñas son distintas");
+        contraseniasIguales=false;
+    }
+
+    return contengaSoloLetras(nombre)&& contengaSoloLetras(apellido) && validarQueSeaCorreo(correo) && queContengaLetrasYNumeros(usuario) && validarContrasenia(contrasenia) 
+    && contraseniasIguales;
+}
+
+function validaMetodoDePago(){
+    //variables de metodo de pago
+    let tarjeta = document.getElementById("numero-tarjeta").value;
+    
+    let titular= document.getElementById("titular").value;
+    
     let clave = document.getElementById("cvv").value;
 
-    let tarjeta = document.getElementById("numero-tarjeta").value;
-
-    let titular= document.getElementById("titular").value;
-
-    //verificar si los campos validados son correctos
-    if(contengaSoloLetras(nombre)&& contengaSoloLetras(apellido) && validarQueSeaCorreo(correo) && queContengaLetrasYNumeros(usuario) && validarContrasenia(contrasenia) 
-        && contrasenia==repeatContrasenia && validarClaveCVV(clave) && validarNumeroTarjeta(tarjeta) && contengaSoloLetras(titular)){
-        formulario.submit();
-    }else{
-        alert("valor invalido");
-        e.preventDefault();
-        enfocarInputsAnterior(inputs);
+    
+    if(clave.toString().trim()==="000"){
+        //done: MENSAJE DE ERROR SI SE COMPLETA CON 000 LA CLAVE TARJETA
+        alert("error cvv no puede ser 000");
     }
-    //si son correctos submitiar el form
+    if(!validarNumeroTarjeta(tarjeta)){
+        //done: mensaje de error si la tarjeta es invalida
+        alert("error tarjeta no valida");
+    }
 
-    //MENSAJE DE ERROR SI SE COMPLETA CON 000 LA CLAVE TARJETA
-
-    //DEBE APARECER UN MENSAJE SIS SON DISTINTAS CONTRASENIAS 
-
-    // mensaje de error si la tarjeta es invalida
-
-});
-
-function validarClaveCVV(clave){
-
-    const validadorCVV= /^[1-9]{3}$/;
-
-    return validadorCVV.test(clave);
-}
-
-function validarNumeroTarjeta(tarjeta) {
-    //valores
-    const validadorTarjeta = /^[0-9]{16,19}$/;
-    let sumaTotal = 0;
-    let ultimoDigito=-1;
-
-    //verificar si es numerico y tiene una longirud pedida
-    if (validadorTarjeta.test(tarjeta)) {
-        //traer todos los numeros de la tarjeta
-        for (let index = 0; index < tarjeta.length - 1; index++) {
-            sumaTotal += parseInt(tarjeta[index]);
-        }
-        //traer el ultimo digito
-        ultimoDigito = parseInt(tarjeta[tarjeta.length-1]);
-    }  
-
-    //comparar 
-    return (sumaTotal % 2 === 0 && ultimoDigito % 2 !== 0)||(sumaTotal % 2 !== 0 && ultimoDigito % 2 === 0);
-}
-
-function contengaSoloLetras(string){
-
-    const expresionLetras=/^[A-Za-z]+$/;
-
-    return expresionLetras.test(string)
-}
-
-function queContengaLetrasYNumeros(valor){
-
-    const expresionLetrasYNumeros= /^[a-zA-Z0-9]+$/;
-
-    return expresionLetrasYNumeros.test(valor);
-}
-
-function validarQueSeaCorreo(correo){
-
-    //valida que tenga arroba y termine en .com
-    //ojo que lo saque de aca https://medium.com/@jgratereaux/validar-correos-electr%C3%B3nicos-con-expresiones-regulares-7914751b6018
-    const validadorCorreo=/^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
-
-    return validadorCorreo.test(correo);
-}
-
-function validarContrasenia(contrasenia){
-
-    const validarRestrinciones= /^(?=.*[A-Za-z].*[A-Za-z])(?=.*\d.*\d)(?=.*[^A-Za-z0-9].*[^A-Za-z0-9]).{8,}$/;
-
-    return validarRestrinciones.test(contrasenia);
+    return validarNumeroTarjeta(tarjeta) && contengaSoloLetras(titular) && validarClaveCVV(clave);
 }
