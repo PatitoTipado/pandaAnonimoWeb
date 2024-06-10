fedf6db64e0cc89a943f10a9e964e774418dcd29
NaN <mauriciodominguez9876543@gmail.com>
2024-06-10 00:24:34 -0300

    comentarios
    Probando la nueva rama, para luego mergear

--- vistaLogin.js
+++ vistaLogin.js
@@ -8,8 +8,10 @@ formulario.addEventListener("submit", (e)=>{
    let contrasenia= document.querySelector("#password").value;
    const inputs = document.querySelectorAll('.espacio');
    
-   if(validadorString(email) && validadorString(contrasenia)){
-    localStorage.setItem(email);
+   //aca tuviera que darle alguna validacion de que el correo ya esta registrado en local y si lo esta
+   if(validadorString(email) && validadorString(contrasenia) ){
+    //aca tuviera que guardar en el local store el correo logeado ponerle un item especial, logeada - el email que se logeo 
+    localStorage.setItem("correo",email);
     formulario.submit();
    }else{
     enfocarInputs(inputs);
